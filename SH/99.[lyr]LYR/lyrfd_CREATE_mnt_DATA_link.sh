39f3058a966d1d30bfa64fa35e734d663948f5d8
lisitsinyr <lisitsinyr@gmail.com>
2024-06-27 09:24:07 +0400

    20240627 92406

--- SH/99.[lyr]LYR/lyrfd_CREATE_mnt_DATA_link.sh
+++ SH/99.[lyr]LYR/lyrfd_CREATE_mnt_DATA_link.sh
@@ -4,7 +4,7 @@
 # -------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
-# procedure CreateLink (Directory)
+# procedure CreateLink (Directory, Link)
 #--------------------------------------------------------------------------------
 function CreateLink () {
 #beginfunction
@@ -13,9 +13,11 @@ function CreateLink () {
     fi
 
     ADirectory=$1
+    ALink=$2
     if [[ -d $ADirectory ]] ; then
         cd ~/Desktop/
-        ln -s $ADirectory
+        rm $ALink
+        ln -s $ADirectory $ALink
     fi
 
     return 0
@@ -27,18 +29,18 @@ function CreateLink () {
     # Создаем каталог /mnt/DATA/ASUS-W10P/WORK
     # ------------------------------------------------------------------
     directory=/mnt/DATA/ASUS-W10P/WORK
-    CreateLink $directory
+    CreateLink $directory WORK
 
     # ------------------------------------------------------------------
     # Создаем каталог /mnt/DATA/ASUS-W10P/SOFT-install
     # ------------------------------------------------------------------
     directory=/mnt/DATA/ASUS-W10P/SOFT-install
-    CreateLink $directory
+    #CreateLink $directory SOFT-install
 
     # ------------------------------------------------------------------
     # Создаем каталог /mnt/DATA/ASUS-W10P/PROJECTS_LYR
     # ------------------------------------------------------------------
     directory=/mnt/DATA/ASUS-W10P/PROJECTS_LYR
-    CreateLink $directory
+    #CreateLink $directory PROJECTS_LYR
 
 #end
