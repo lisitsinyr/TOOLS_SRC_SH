06e6ee9a7687843e144a8494e6041954a2e8ff7b
lisitsinyr <lisitsinyr@gmail.com>
2024-08-04 09:25:34 +0400

    20240804 92534

---
+++ SH/99.[lyr]LYR/lyrfd_CREATE_DESKTOP.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# -------------------------------------------------------------------
+# lyrfd_CREATE_DESKTOP.sh
+# -------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# procedure CreateLink (Object, Link)
+#--------------------------------------------------------------------------------
+function CreateLink () {
+#beginfunction
+    if [[ "$DEBUG" -eq 1 ]] ; then
+        echo DEBUG: function $FUNCNAME ... >$(tty)
+    fi
+
+    AObject=$1
+    ALink=$2
+    if [[ -d $Object ]] ; then
+        cd ~/Desktop/
+        rm $ALink
+        ln -s $Object $ALink
+    fi
+    if [[ -f $Object ]] ; then
+        cd ~/Desktop/
+        rm $ALink
+        ln -s $Object $ALink
+    fi
+
+    return 0
+}
+#endfunction
+
+#begin
+    # ------------------------------------------------------------------
+    # 
+    # ------------------------------------------------------------------
+    DIR_SH=~/PROJECTS_LYR/CHECK_LIST/01_OS/03_UNIX/PROJECTS_UNIX/TOOLS_SRC_SH
+
+    PROJECTS_LYR_PULL=$DIR_SH/SH/99.[lyr]LYR/PROJECTS_LYR_PULL.sh
+    CreateLink $PROJECTS_LYR_PULL PROJECTS_LYR_PULL
+
+    PROJECTS_LYR_PUSH=$DIR_SH/SH/99.[lyr]LYR/PROJECTS_LYR_PUSH.sh
+    CreateLink $PROJECTS_LYR_PUSH PROJECTS_LYR_PUSH
+
+    return 0
+#end
