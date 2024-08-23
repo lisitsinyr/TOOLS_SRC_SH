3283142c11c409b653e01a8388afc95026ed7b06
lisitsinyr <lisitsinyr@gmail.com>
2024-08-23 18:25:51 +0400

    20240823182551

--- SH/99.[lyr]LYR/[lyrxxx_]PATTERN.sh
+++ SH/99.[lyr]LYR/[lyrxxx_]PATTERN.sh
@@ -4,6 +4,37 @@
 # -----------------------------------------------
 
 # -----------------------------------------------
+# procedure INIT_PROJECTS_LYR_DIR ()
+# -----------------------------------------------
+function INIT_PROJECTS_LYR_DIR () {
+#beginfunction
+    if [[ "$DEBUG" -eq 1 ]] ; then
+        echo DEBUG: procedure $FUNCNAME ... >$(tty)
+    fi
+
+    # -------------------------------------------------------------------
+    # PROJECTS_LYR_DIR - каталог проектов LYR
+    # -------------------------------------------------------------------
+    UNAME=$(uname -n)
+    case "$UNAME" in
+        'ASUS-W10P')
+            PROJECTS_LYR_DIR='/d/PROJECTS_LYR'
+            ;;
+        'ASUS-U2204-VB' | 'ASUS-U2204-VM' | 'ASUS-U2404-VB' | 'ASUS-U2404-VM' | 'ASUS-U2310')
+            PROJECTS_LYR_DIR='/home/lyr/PROJECTS_LYR'
+        ;;
+        *)
+            echo "ERROR: Компьютер не определен...!"
+            exit 1
+            ;;
+    esac
+    #echo PROJECTS_LYR_DIR:$PROJECTS_LYR_DIR
+
+    return 0
+}
+#endfunction
+
+# -----------------------------------------------
 # procedure INIT_PROJECT ()
 # -----------------------------------------------
 function INIT_PROJECT () {
@@ -13,24 +44,6 @@ function INIT_PROJECT () {
     fi
 
     if [[ -z $INIT_PROJECT ]] ; then
-        # -------------------------------------------------------------------
-        # PROJECTS_LYR_DIR - каталог проектов LYR
-        # -------------------------------------------------------------------
-        UNAME=$(uname -n)
-        case "$UNAME" in
-            'ASUS-W10P')
-                PROJECTS_LYR_DIR='/d/PROJECTS_LYR'
-                ;;
-            'ASUS-U2204-VB' | 'ASUS-U2204-VM' | 'ASUS-U2404-VB' | 'ASUS-U2404-VM' | 'ASUS-U2310')
-                PROJECTS_LYR_DIR='/home/lyr/PROJECTS_LYR'
-            ;;
-            *)
-                echo "ERROR: Компьютер не определен...!"
-                exit 1
-                ;;
-        esac
-        #echo PROJECTS_LYR_DIR:$PROJECTS_LYR_DIR
-
         #-------------------------------------------------------------------
         # Инициализация проекта PATTERN
         #-------------------------------------------------------------------
@@ -45,6 +58,42 @@ function INIT_PROJECT () {
             echo INFO: Файл $LPROJECTSET не существует...
         fi
         INIT_PROJECT=1
+    fi
+
+    return 0
+}
+#endfunction
+
+# -----------------------------------------------
+# procedure INIT_LIB (SCRIPT)
+# -----------------------------------------------
+function INIT_LIB () {
+#beginfunction
+    if [[ "$DEBUG" -eq 1 ]] ; then
+        echo DEBUG: procedure $FUNCNAME ... >$(tty)
+    fi
+
+    if [[ -z $INIT_LIB ]] ; then
+        # -------------------------------------------------------------------
+        # SCRIPTS_DIR - каталог скриптов
+        # -------------------------------------------------------------------
+        SCRIPTS_DIR="$PROJECTS_LYR_DIR"'/CHECK_LIST/01_OS/03_UNIX/PROJECTS_UNIX/TOOLS_SRC_SH'
+        echo SCRIPTS_DIR:$SCRIPTS_DIR
+        # -------------------------------------------------------------------
+        # LIB_SH - каталог библиотеки скриптов
+        # -------------------------------------------------------------------
+        if [[ -z "$LIB_SH" ]] ; then
+            LIB_SH="$SCRIPTS_DIR/LIB"
+            echo LIB_SH:$LIB_SH
+        fi
+        if [[ ! -d "$LIB_SH" ]] ; then
+            echo ERROR: Каталог библиотеки LYR $LIB_SH не существует...
+            exit 1
+        fi
+        # -------------------------------------------------------------------
+        # Инициализация БИБЛИОТЕКИ LYR
+        # -------------------------------------------------------------------
+        source "$LIB_SH/LYRINIT.sh" "$1"
     fi
 
     return 0
@@ -137,30 +186,17 @@ function MAIN () {
     DEBUG=0
 
     #-------------------------------------------------------------------
+    # Установка PROJECTS_LYR_DIR
+    #-------------------------------------------------------------------
+    INIT_PROJECTS_LYR_DIR
+    #-------------------------------------------------------------------
     # Установка проекта PATTERN
     #-------------------------------------------------------------------
     INIT_PROJECT
-
     #-------------------------------------------------------------------
     # Установка LIB LYR
     #-------------------------------------------------------------------
-    LINIT_LIB="$PROJECT_DIR"/'INIT_LIB.sh'
-    if [[ -r "$LINIT_LIB" ]] ; then
-        source "$LINIT_LIB"
-        INIT_LIB "$0"
-    else
-        #-------------------------------------------------------------------
-        # Текущий каталог
-        #-------------------------------------------------------------------
-        LINIT_LIB='./INIT_LIB.sh'
-        if [[ -r "$LINIT_LIB" ]] ; then
-            source "$LINIT_LIB"
-            INIT_LIB "$0"
-        else
-            echo ERROR: Файл $LINIT_LIB не существует...
-            exit 1
-        fi
-    fi
+    INIT_LIB "$0"
 
     # Количество аргументов
     Read_N
--- SH/99.[lyr]LYR/[lyrxxx_]PATTERN_empty.sh
+++ SH/99.[lyr]LYR/[lyrxxx_]PATTERN_empty.sh
@@ -4,28 +4,24 @@
 # -----------------------------------------------
 
 # -----------------------------------------------
-# procedure INIT_LIB (SCRIPT)
+# procedure INIT_PROJECTS_LYR_DIR ()
 # -----------------------------------------------
-function INIT_LIB () {
+function INIT_PROJECTS_LYR_DIR () {
 #beginfunction
-    #echo DEBUG: $DEBUG
     if [[ "$DEBUG" -eq 1 ]] ; then
         echo DEBUG: procedure $FUNCNAME ... >$(tty)
     fi
 
     # -------------------------------------------------------------------
-    # SCRIPTS_DIR - каталог скриптов
+    # PROJECTS_LYR_DIR - каталог проектов LYR
     # -------------------------------------------------------------------
     UNAME=$(uname -n)
-    #echo UNAME:$UNAME
     case "$UNAME" in
         'ASUS-W10P')
             PROJECTS_LYR_DIR='/d/PROJECTS_LYR'
-            SCRIPTS_DIR='/d/PROJECTS_LYR/CHECK_LIST/01_OS/03_UNIX/PROJECTS_UNIX/TOOLS_SRC_SH'
             ;;
         'ASUS-U2204-VB' | 'ASUS-U2204-VM' | 'ASUS-U2404-VB' | 'ASUS-U2404-VM' | 'ASUS-U2310')
             PROJECTS_LYR_DIR='/home/lyr/PROJECTS_LYR'
-            SCRIPTS_DIR='/home/lyr/PROJECTS_LYR/CHECK_LIST/01_OS/03_UNIX/PROJECTS_UNIX/TOOLS_SRC_SH'
         ;;
         *)
             echo "ERROR: Компьютер не определен...!"
@@ -33,22 +29,6 @@ function INIT_LIB () {
             ;;
     esac
     #echo PROJECTS_LYR_DIR:$PROJECTS_LYR_DIR
-    #echo SCRIPTS_DIR:$SCRIPTS_DIR
-    # -------------------------------------------------------------------
-    # LIB_SH - каталог библиотеки скриптов
-    # -------------------------------------------------------------------
-    if [[ -z "$LIB_SH" ]] ; then
-        LIB_SH="$SCRIPTS_DIR/LIB"
-        #echo LIB_SH:$LIB_SH
-    fi
-    if [[ ! -d "$LIB_SH" ]] ; then
-        echo ERROR: Каталог библиотеки LYR $LIB_SH не существует...
-        exit 1
-    fi
-    # -------------------------------------------------------------------
-    # Инициализация БИБЛИОТЕКИ LYR
-    # -------------------------------------------------------------------
-    source "$LIB_SH/LYRINIT.sh" "$1"
 
     return 0
 }
@@ -64,24 +44,6 @@ function INIT_PROJECT () {
     fi
 
     if [[ -z $INIT_PROJECT ]] ; then
-        # -------------------------------------------------------------------
-        # PROJECTS_LYR_DIR - каталог проектов LYR
-        # -------------------------------------------------------------------
-        UNAME=$(uname -n)
-        case "$UNAME" in
-            'ASUS-W10P')
-                PROJECTS_LYR_DIR='/d/PROJECTS_LYR'
-                ;;
-            'ASUS-U2204-VB' | 'ASUS-U2204-VM' | 'ASUS-U2404-VB' | 'ASUS-U2404-VM' | 'ASUS-U2310')
-                PROJECTS_LYR_DIR='/home/lyr/PROJECTS_LYR'
-            ;;
-            *)
-                echo "ERROR: Компьютер не определен...!"
-                exit 1
-                ;;
-        esac
-        #echo PROJECTS_LYR_DIR:$PROJECTS_LYR_DIR
-
         #-------------------------------------------------------------------
         # Инициализация проекта PATTERN
         #-------------------------------------------------------------------
@@ -102,32 +64,54 @@ function INIT_PROJECT () {
 }
 #endfunction
 
-#begin
-    #-------------------------------------------------------------------
-    #
-    #-------------------------------------------------------------------
-    INIT_PROJECT
+# -----------------------------------------------
+# procedure INIT_LIB (SCRIPT)
+# -----------------------------------------------
+function INIT_LIB () {
+#beginfunction
+    if [[ "$DEBUG" -eq 1 ]] ; then
+        echo DEBUG: procedure $FUNCNAME ... >$(tty)
+    fi
 
-    #-------------------------------------------------------------------
-    # Каталог проекта
-    #-------------------------------------------------------------------
-    LINIT_LIB="$PROJECT_DIR"/'INIT_LIB.sh'
-    if [[ -r "$LINIT_LIB" ]] ; then
-        source "$LINIT_LIB"
-        INIT_LIB "$0"
-    else
-        #-------------------------------------------------------------------
-        # Текущий каталог
-        #-------------------------------------------------------------------
-        LINIT_LIB='./INIT_LIB.sh'
-        if [[ -r "$LINIT_LIB" ]] ; then
-            source "$LINIT_LIB"
-            INIT_LIB "$0"
-        else
-            echo ERROR: Файл $LINIT_LIB не существует...
+    if [[ -z $INIT_LIB ]] ; then
+        # -------------------------------------------------------------------
+        # SCRIPTS_DIR - каталог скриптов
+        # -------------------------------------------------------------------
+        SCRIPTS_DIR="$PROJECTS_LYR_DIR"'/CHECK_LIST/01_OS/03_UNIX/PROJECTS_UNIX/TOOLS_SRC_SH'
+        echo SCRIPTS_DIR:$SCRIPTS_DIR
+        # -------------------------------------------------------------------
+        # LIB_SH - каталог библиотеки скриптов
+        # -------------------------------------------------------------------
+        if [[ -z "$LIB_SH" ]] ; then
+            LIB_SH="$SCRIPTS_DIR/LIB"
+            echo LIB_SH:$LIB_SH
+        fi
+        if [[ ! -d "$LIB_SH" ]] ; then
+            echo ERROR: Каталог библиотеки LYR $LIB_SH не существует...
             exit 1
         fi
+        # -------------------------------------------------------------------
+        # Инициализация БИБЛИОТЕКИ LYR
+        # -------------------------------------------------------------------
+        source "$LIB_SH/LYRINIT.sh" "$1"
     fi
+
+    return 0
+}
+#endfunction
+
+#begin
+    # Установка PROJECTS_LYR_DIR
+    #-------------------------------------------------------------------
+    INIT_PROJECTS_LYR_DIR
+    #-------------------------------------------------------------------
+    # Установка проекта PATTERN
+    #-------------------------------------------------------------------
+    INIT_PROJECT
+    #-------------------------------------------------------------------
+    # Установка LIB LYR
+    #-------------------------------------------------------------------
+    INIT_LIB "$0"
 
     # Количество аргументов
     Read_N
