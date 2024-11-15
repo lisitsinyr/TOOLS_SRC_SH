18967e987adb85138d0cc9fe923572b03740873d
lisitsinyr <lisitsinyr@gmail.com>
2024-11-15 14:42:40 +0400

    20241115144239

--- SRC/SH/99.[lyr]LYR/[lyrxxx_]PATTERN_empty.sh
+++ SRC/SH/99.[lyr]LYR/[lyrxxx_]PATTERN_empty.sh
@@ -3,6 +3,22 @@
 # [lyrxxx_]PATTERN_empty.sh
 # -----------------------------------------------
 
+#--------------------------------------------------------------------------------
+# procedure MAIN ()
+#--------------------------------------------------------------------------------
+function MAIN () {
+#beginfunction
+    if [[ "$DEBUG" -eq 1 ]] ; then
+        echo DEBUG: procedure $FUNCNAME ... >$(tty)
+    fi
+
+    BATNAME=$0
+    echo Start $BATNAME ...
+
+    return 0
+}
+#endfunction
+
 #begin
     LFileNameINIT='INIT_TOOLS_SRC_SH.sh'
     if [[ -r "$LFileNameINIT" ]] ; then
@@ -13,9 +29,6 @@
         exit 1
     fi
 
+    MAIN
     read -n 1 -s -r -p $'Press any key to continue ...\n'
-
-    return 0
 #end
-
-# ===================================================================
